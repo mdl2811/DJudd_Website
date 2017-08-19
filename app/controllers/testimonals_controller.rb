7b5ce2d6e43ec11b796119ebeddd4aded9cfb7a5
Michael LaBahn <mdlabahn@yahoo.com>
2017-08-18 19:40:26 -0600

    finished controllers

--- app/controllers/testimonals_controller.rb
+++ app/controllers/testimonals_controller.rb
@@ -4,12 +4,40 @@ class TestimonalsController < ApplicationController
   end
 
   def show
-    
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_parmas)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @user.update(user_parmas)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    redirect_to home
+  end
+
+  private
+
+  def user_parmas
+    params.require(:user)
   end
 end
