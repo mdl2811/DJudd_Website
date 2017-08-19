b4667719c80086e66ef121605214b6416bc65d53
Michael LaBahn <mdlabahn@yahoo.com>
2017-08-18 22:29:15 -0600

    more auth stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  devise_for :users, controllers: {registrations: 'users/registrations'
-}
+  get 'omniauth_callbacks/index'
+
+    root 'categories#index'
+    devise_for :users, :controllers => {:omniauth_callbacks => "omniauth_callbacks"}
 
   get 'testimonals/index'
 
